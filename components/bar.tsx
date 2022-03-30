0885b0b856cb5d76a690513ff1f857b89c4c6ce9
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-31 00:01:10 +0530

    install react types, format code

--- components/bar.tsx
+++ components/bar.tsx
@@ -1,41 +1,58 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const Bar = ({ update }: any) => {
+  const [todo, setTodo] = useState("");
 
-    const [todo, setTodo] = useState("");
+  function addTodo() {
+    setTodo("");
 
-    function addTodo() {
-        setTodo("")
-        if (todo.length < 0) { return }
-
-        let data = localStorage.getItem("todos");
-
-        let json;
-
-        if (!data) {
-            json = {};
-        } else {
-            json = JSON.parse(data);
-        }
-        json[`${todo}`] = false;
-        let result = [];
-        for (const key in json) {
-            if (json.hasOwnProperty(key)) {
-                result.push([key, json[key]]);
-            }
-        }
-        localStorage.setItem(`todos`, JSON.stringify(json));
-        update(result);
+    if (todo.length < 0) {
+      return;
     }
 
+    let data = localStorage.getItem("todos");
 
-    return (
-        <>
-            <div className="bar">
-                <input type="text" placeholder="Add a todo" className=" block" value={todo} onChange={(e) => setTodo(e.target.value)} />
-                <button type='button' className=" add-btn block text-white h-[50px] font-bold rounded m-0 p-0 w-[600px] " onClick={addTodo} disabled={todo.length > 0 ? false : true}>ADD</button>
-            </div>
-        </>
-    )
+    let json;
 
-}
+    if (!data) {
+      json = {};
+    } else {
+      json = JSON.parse(data);
+    }
+
+    json[`${todo}`] = false;
+
+    let result = [];
+
+    for (const key in json) {
+      if (json.hasOwnProperty(key)) {
+        result.push([key, json[key]]);
+      }
+    }
+
+    localStorage.setItem(`todos`, JSON.stringify(json));
+    update(result);
+  }
+
+  return (
+    <>
+      <div className="bar">
+        <input
+          type="text"
+          placeholder="Add a todo"
+          className=" block"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button
+          type="button"
+          className="add-btn m-0 block h-[50px] w-[600px] rounded p-0 font-bold text-white"
+          onClick={addTodo}
+          disabled={todo.length > 0 ? false : true}
+        >
+          ADD
+        </button>
+      </div>
+    </>
+  );
+};
