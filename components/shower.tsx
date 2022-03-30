e0e261795fab78c6225a65b83a4e95b428b5bdb2
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-31 00:05:09 +0530

    format more code

--- components/shower.tsx
+++ components/shower.tsx
@@ -1,62 +1,66 @@
-import { useEffect, useRef, useState } from "react"
 import { Todo } from "./todo";
 
 export const Shower = ({ todos, update }: any) => {
+  function change(e: any) {
+    let data = localStorage.getItem("todos");
 
-    function change(e: any) {
+    if (!data) {
+      update([]);
+    } else {
+      let result = [];
+      let json = JSON.parse(data);
+      json[`${e.target.id}`] = e.target.checked;
+
+      for (const key in json) {
+        if (json.hasOwnProperty(key)) {
+          result.push([key, json[key]]);
+        }
+      }
+
+      localStorage.setItem(`todos`, JSON.stringify(json));
+      update(result);
+    }
+  }
+
+  const deel = {
+    name: "",
+    _delete: function (name: string) {
+      const t = name;
+      return function () {
         let data = localStorage.getItem("todos");
 
         if (!data) {
-            update([]);
+          update([]);
         } else {
-            let result = [];
-            let json = JSON.parse(data)
-            json[`${e.target.id}`] = e.target.checked;
-            for (const key in json) {
-                if (json.hasOwnProperty(key)) {
-                    result.push([key, json[key]]);
-                }
+          let result = [];
+          let json = JSON.parse(data);
+
+          delete json[`${t}`];
+
+          for (const key in json) {
+            if (json.hasOwnProperty(key)) {
+              result.push([key, json[key]]);
             }
-            localStorage.setItem(`todos`, JSON.stringify(json));
-            update(result);
+          }
+
+          localStorage.setItem(`todos`, JSON.stringify(json));
+          update(result);
         }
+      };
     }
+  };
 
-    const deel = {
-        name: "",
-        _delete: function (name: string) {
-            const t = name;
-            return function () {
-                let data = localStorage.getItem("todos");
-
-                if (!data) {
-                    update([]);
-                } else {
-                    let result = [];
-                    let json = JSON.parse(data);
-                    delete json[`${t}`];
-                    for (const key in json) {
-                        if (json.hasOwnProperty(key)) {
-                            result.push([key, json[key]]);
-                        }
-                    }
-                    localStorage.setItem(`todos`, JSON.stringify(json));
-                    update(result);
-                }
-            }
-        }
-    }
-
-    return (
-        <div className=" shower flex text-white mt-[100px] items-center justify-center">
-            {todos.map((todo: Array<any>, index: number) => {
-                let d = deel;
-                deel.name = todo[0];
-                // console.log(deel);
-                return (<div className="todos" >
-                    <Todo todo={todo} change={change} del={d} />
-                </div>);
-            })}
-        </div >
-    )
-}
+  return (
+    <div className="shower mt-[100px] flex items-center justify-center text-white">
+      {todos.map((todo: Array<any>, index: number) => {
+        let d = deel;
+        deel.name = todo[0];
+        return (
+          <div className="todos">
+            <Todo todo={todo} change={change} del={d} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
--- components/todo.tsx
+++ components/todo.tsx
@@ -1,12 +1,18 @@
-import { useRef } from "react"
-
 export const Todo = ({ todo, change, del }: any) => {
-
-    return <div className="todo" >
-        <div className="todo-stuffs">
-            <input type="checkbox" checked={todo[1]} onChange={change} id={todo[0]} />
-            <label>{todo}</label>
-        </div>
-        <button type="button" onClick={del._delete(todo[0])}>Delete</button>
+  return (
+    <div className="todo">
+      <div className="todo-stuffs">
+        <input
+          type="checkbox"
+          checked={todo[1]}
+          onChange={change}
+          id={todo[0]}
+        />
+        <label>{todo}</label>
+      </div>
+      <button type="button" onClick={del._delete(todo[0])}>
+        Delete
+      </button>
     </div>
-}
+  );
+};
